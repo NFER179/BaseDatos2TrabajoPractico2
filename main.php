36a7f7a332dd04f6c0d8fe76d8cf533aacc1e58c
n.fernandez <nikin179@hotmail.com>
2017-06-20 19:42:23 -0300

    Update main.php

--- main.php
+++ main.php
@@ -1,9 +1,16 @@
 <html>
   <?php
     include 'phpFunctions/DBManage.php';
+    include 'phpFunctions/MainFunc.php';
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+      if (!empty($_POST["peliculaInt"])) {
+        $_saveStatus = GuardarPeliculasVistas($_GET["usuario"], $_POST["peliculaInt"]);
+      }
+    }
   ?>
   <head>
-    <link rel="stylesheet" type="text/css" href="css/estilos.css"></script>
+    <link rel="stylesheet" type="text/css" href="css/defaultStyle.css"></script>
     <link rel="stylesheet" type="text/css" href="css/mainStyle.css"></script>
 
     <script type="text/javascript" src="js/functions.js"></script>
@@ -19,28 +26,38 @@
           <t><?php echo $_nombreCompleto?></t>
           <input type="submit" value="Cambiar Contrase&#241a">
         </form>
+        <a href="index.php">Log Out</a>
       </div>
       <div id="cuerpoDiv">
         <div id="agregarPeliDiv">
-          <input type="text" id="peliculaInt" name="peliculaInt" onkeypress="HandleKeyPress(event)">
-          <input type="button" value="Agregar" onclick="AgregarPelicula()">
-          <form method="post" action="">
-            <table id="pelisAgregadasTbl">
-              <tr>
-                <th>Peliculas</th>
-              </tr>
-            </table>
+          <form method="post" action="<?php echo htmlspecialchars($_SEVER["PHP_SELF"]);?>">
+<!--            <input type="text" id="peliculaInt" name="peliculaInt" onkeypress="HandleKeyPress(event)">            <input type="button" value="Agregar" onclick="AgregarPelicula()">-->
+            <input type="text" name="peliculaInt">
+            <input type="submit" value="Agregar">
           </form>
+          <?php
+            if(!empty($_saveStatus)) {
+              ?>
+              <label><?php echo $_saveStatus?></label>
+              <?php
+            }
+          ?>
+<!--          <form method="post" action="<?php echo htmlspecialchars($_SEVER["PHP_SELF"]);?>">            <table id="pelisAgregadasTbl">              <tr>                <th>Peliculas</th>              </tr>            </table>            <input type="button" id="GuardarPeliculas" value="Guardar">          </form> -->
         </div>
         <div id="pelisVistasDiv">
           <table id="pelisVistasTbl">
             <tr>
-              <th>Peliculas que Vi<th>
+              <th>Peliculas Vistas</th>
             </tr>
+            <?php
+              PeliculasVistas($_GET["usuario"]);
+            ?>
           </table>
         </div>
         <div id="paresUsuariosDiv">
-          <table> nico </table>
+          <?php
+            ArmarTablaPeliculasVistas();
+          ?>	
         </div>
       <div>
       <div id="pieDiv">
