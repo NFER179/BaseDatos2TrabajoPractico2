98bc716efb5c478ab89115cd45b546d3420d2003
n.fernandez <nikin179@hotmail.com>
2017-06-24 00:26:22 -0300

    Add files via upload

--- phpFunctions/Encriptador.php
+++ phpFunctions/Encriptador.php
@@ -4,3 +4,4 @@
     return md5($key);
   }
 ?>
+
--- phpFunctions/MainFunc.php
+++ phpFunctions/MainFunc.php
@@ -12,27 +12,41 @@
   function PeliculasVistas($usuario) {
     $_pelisvistas = PelisQueVio($usuario);
 
+    $_n = 1;
+    
+
+    echo "<tbody>";
     while($_row = pg_fetch_row($_pelisvistas)) {
-      echo "<tr>";
+      echo $_n%2==0 ? "<tr class='alt'>" : "<tr>";
       echo "<td>" . $_row[0] . "</td>";
+//      echo "<td style='width:10px;height:10px;'><input type='button' value='-'></td>";
       echo "</tr>";
+      $_n = $_n + 1;
     }
+    echo "<tbody>";
   }
 
   function ArmarTablaPeliculasVistas(){
     $_paresPorPelicula = ParesPorPelicula();
 
     echo "<table>";
-    echo "<tr><th colspan='2'>Pares que vieron las mismas peliculas</th></tr>";
-    echo "<tr> <th>Usuario1</th> <th>Usuario2</th> </tr>";
+    echo "<thead>";
+    echo "<tr><th colspan='2' align='center'>Pares que vieron las mismas peliculas</th></tr>";
+    echo "<tr> <th align='center'>Usuario1</th> <th align='center'>Usuario2</th> </tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    $_n = 1;
 
     while($_row = pg_fetch_row($_paresPorPelicula)) {
-      echo "<tr>";
+      echo $_n%2==0 ? "<tr class='alt'>" : "<tr>";
       echo "<td>" . $_row[0] . "</td><td>" . $_row[1] . "</td>";
       echo "</tr>";
     }
 
+    echo "<tbody>";
     echo "</table>";
     
   }
 ?>
+
